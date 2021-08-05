cf2217868433e1d4df680d2cc7999a7999eb71e9
piro <piro.haniwa@gmail.com>
2021-08-05 18:13:39 +0900

    fix🐛:  set base url

--- src/libs/axiosInstance/index.ts
+++ src/libs/axiosInstance/index.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: "http://localhost:3000/api" });
+const axiosInstance = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api`,
+});
 
 export default axiosInstance;
